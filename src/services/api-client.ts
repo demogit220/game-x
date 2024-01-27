d78f18c96cafb6d2bf3b987bebdfe43b5a0feaf6
demogit220 <gauravkumar_20329@aitpune.edu.in>
2024-01-27 13:07:42 +0530

    Refactroing fetch games with react query and platform filter issue solved

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
+
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: 'b72145025be04c959877dbcd4a792862'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "b72145025be04c959877dbcd4a792862",
+  },
+});
