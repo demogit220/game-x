8750dc50e20350de11f52c78aefabb54d1330863
demogit220 <gauravkumar_20329@aitpune.edu.in>
2024-01-28 06:02:33 +0530

    Setup - routing

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,25 +1,21 @@
-import { Grid, GridItem, Show, HStack } from "@chakra-ui/react";
-import GameGrid from "./components/GameGrid";
-import GenreList from "./components/GenreList";
-import NavBar from "./components/NavBar";
-import PlatformSelector from "./components/PlatformSelector";
-import SortSelector from "./components/SortSelector";
+import { Grid, Show, GridItem, HStack } from "@chakra-ui/react";
+import GameGrid from "../components/GameGrid";
+import GenreList from "../components/GenreList";
+import PlatformSelector from "../components/PlatformSelector";
+import SortSelector from "../components/SortSelector";
 
-function App() {
+const Homepage = () => {
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
+        base: `"main"`,
+        lg: `"aside main"`,
       }}
       templateColumns={{
         base: "1fr", // 1fr means 1 fraction that is whole available space
         lg: "200px 1fr",
       }}
     >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
           <GenreList />
@@ -35,6 +31,6 @@ function App() {
       {/* <GridItem area='main' bg='yellow'>main</GridItem> */}
     </Grid>
   );
-}
+};
 
-export default App;
+export default Homepage;
